7bd67809c721fcef20eb1ac06fa45714a07d509e
nayeong lee <lny021102@gmail.com>
2023-11-18 17:27:20 +0900

    [PROGRESS] webcam 및 이미지 stack 구현, 드래그 오류 수정 중

--- src/Pages/Camera.jsx
+++ src/Pages/Camera.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import Webcam from 'react-webcam';
 import styled from 'styled-components';
 
 const Section = styled.section`
@@ -10,7 +12,7 @@ const Section = styled.section`
   padding: 12px 4px;
 `;
 
-const ImageContainer = styled.div`
+const WebcamContainer = styled.div`
   position: relative;
   width: 100%;
   max-width: 400px;
@@ -18,23 +20,9 @@ const ImageContainer = styled.div`
   border: 1px solid white;
   border-radius: 8px;
   overflow: hidden;
-`;
-
-const Placeholder = styled.span`
-  width: 100%;
-  height: 100%;
-  background-color: #e2e8f0;
-  border-radius: 4px;
-`;
-
-const StyledImg = styled.img`
-  position: absolute;
-  top: 16px;
-  left: 16px;
-  width: 96px;
-  height: 96px;
-  object-fit: cover;
-  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ScrollableImageList = styled.div`
@@ -74,24 +62,62 @@ const RoundButton = styled.button`
   border-width: 4px;
 `;
 
-
+//카메라 위에 뜰 이미지 컴포넌트 추가함
+const StyledImg = styled.img`
+  position: absolute;
+  width: 96px;
+  height: 96px;
+  object-fit: cover;
+  border-radius: 4px;
+  z-index: 1;
+  transform: translate(${({ imgPos }) => imgPos.x}px, ${({ imgPos }) => imgPos.y}px);
+`;
 
 export default function Component() {
+  const [dragging, setDragging] = useState(false);
+  const [imgPos, setImgPos] = useState({ x: 16, y: 16 });
+
+  const handleMouseDown = (e) => {
+    setDragging(true);
+  };
+
+  const handleMouseMove = (e) => {
+    if (dragging && e.clientX && e.clientY) {
+      const boundingRect = e.target.getBoundingClientRect();
+      const newX = e.clientX - boundingRect.left - 96 / 2;
+      const newY = e.clientY - boundingRect.top - 96 / 2;
+
+      setImgPos({ x: newX, y: newY });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+  };
+
   return (
     <Section>
-      <ImageContainer>
-        <Placeholder />
+      <WebcamContainer
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        <Webcam
+          screenshotFormat="image/jpeg"
+          style={{ width: '100%', height: '100%', position: 'absolute' }}
+        />
         <StyledImg
           alt="AR Tattoo"
           draggable="true"
           src="/likelion.png"
           style={{ aspectRatio: '100/100', objectFit: 'cover' }}
+          imgPos={imgPos}
         />
-      </ImageContainer>
+      </WebcamContainer>
       <ScrollableImageList aria-label="Scrollable Image List">
         <ImageGroup>
-            <img src="/likelion.png"/>
-            <img src="/likelion.png"/>
+          <img src="/likelion.png" alt="Image 1" />
+          <img src="/likelion.png" alt="Image 2" />
         </ImageGroup>
       </ScrollableImageList>
       <ActionButtons>
