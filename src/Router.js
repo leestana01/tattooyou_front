34e3cddfa8b0508c258b106d0853530c575db205
SangYoonLee <prestro1231@hufs.ac.kr>
2023-11-18 21:26:45 +0900

    Modify: 11/18 작업 - 배경 화면 수정 (애니메이션 구현) - 로그인 페이지에서 엔터키 작동하도록 수정 (Form 합침) - 메뉴 화면 디자인 조금 수정 (세부적인 거라 티는 잘 안남) - 타투샵 페이지 생성 + 라우터에 추가 (Contest 페이지 코드를 그대로 가져온 후 문구만 수정해둠) - MainPage 디렉토리 이동으로 인해 생긴 오류 전부 수정 (import 코드 전부 수정)

--- src/Router.js
+++ src/Router.js
@@ -1,39 +1,44 @@
 import React from "react";
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "./Pages/Main";
-import Login from "./Components/MainPage/Login";
-import Menus from "./Components/MainPage/Menus";
-import MyPage from "./Components/MainPage/MyPage";
-import Contest from "./Components/MainPage/Contest"
-import Camera from "./Pages/Camera"
+import Login from "./Pages/MainPage/Login";
+import Menus from "./Pages/MainPage/Menus";
+import MyPage from "./Pages/MainPage/MyPage";
+import Contest from "./Pages/MainPage/Contest";
+import TattooShop from "./Pages/MainPage/TattooShop";
+import Camera from "./Pages/Camera";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Main />,
-        children: [
-            {
-                path: "",
-                element: <Login/>
-            },
-            {
-                path: "/menus",
-                element: <Menus/>
-            },
-            {
-                path: "/mypage",
-                element:<MyPage />
-            },
-            {
-                path: "/contest",
-                element:<Contest/>
-            }
-        ]
-    },
-    {
-        path: "/camera",
-        element: <Camera/>
-    }
-])
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "",
+        element: <Login />,
+      },
+      {
+        path: "/menus",
+        element: <Menus />,
+      },
+      {
+        path: "/mypage",
+        element: <MyPage />,
+      },
+      {
+        path: "/contest",
+        element: <Contest />,
+      },
+      {
+        path: "/tattoo-shop",
+        element: <TattooShop />,
+      },
+    ],
+  },
+  {
+    path: "/camera",
+    element: <Camera />,
+  },
+]);
 
 export default router;
